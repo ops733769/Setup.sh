9d12e232986cdf706c221d6e925d0695fa113504
ops733769 <154208323+ops733769@users.noreply.github.com>
2024-03-25 13:05:44 +0800

    Create option.sh

---
+++ option.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# 获取系统基础信息并保存到日志文件
+{
+    echo "=== 操作系统版本 ==="
+    cat /etc/os-release
+
+    echo -e "\n=== 内核版本 ==="
+    uname -a
+
+    echo -e "\n=== 主机名 ==="
+    hostname
+
+    echo -e "\n=== 当前登录用户 ==="
+    whoami
+    
+     echo -e "\n=== 系统启动时间和负载情况 ==="
+     uptime
+     
+     echo -e "\n=== CPU 信息 ==="
+     lscpu
+     
+      echo -e "\n=== 内存使用情况 ==="
+      free -h
+      
+       echo -e "\n=== 磁盘空间使用情况==="
+       df 
+} > system_inspection.log  # 将输出重定向到日志文件中
